1ad89c9d665b484e27c46d3bb650132a3bb394ee
Romano Kurt Maynardson <kmromano911@gmail.com>
2026-04-14 15:06:30 +0800

    Added more items, user input, and basic purchase logic.

--- Program.cs
+++ Program.cs
@@ -18,20 +18,74 @@
         static void Main(string[] args)
         {
             Console.WriteLine("ZAWARUDOOO");
-            // SAMPLE ITEM AND TEST
-            Item i1 = new Item();
-            i1.Id = 1;
-            i1.Name = "Hany";
-            i1.Price = 4;
-            i1.Stock = 67;
 
-            i1.DisplayItems();
-            Console.WriteLine("Name: " + i1.Name);
-            Console.WriteLine("Item ID: " + i1.Id);
-            Console.WriteLine("Price: " + i1.Price);
-            Console.WriteLine("Stock: " + i1.Stock);
+            //################################################### ITEMS AREA ########################################################
 
+            Item[] items = new Item[]
+            {
+                new () { Id = 1, Name = "Hany", Price = 4, Stock = 24},
+                new () { Id = 2, Name = "Karate Belt", Price = 2, Stock = 30},
+                new () { Id = 3, Name = "Attack Helicopter", Price = 40000000, Stock = 4},
+                new () { Id = 5, Name = "Frutos", Price = 1, Stock = 10},
+            };
 
+            //################################################### DISPLAY ITEMS ########################################################
+
+            Console.WriteLine("--------- ITEMS 4 SALE ---------");
+            Console.WriteLine(" | ID | Name       | Price   | Stock |");
+            Console.WriteLine("--------------------------------------");
+
+            foreach (Item item in items)
+            {
+                item.DisplayItems();
+            }
+
+            //################################################### CART AREA ########################################################
+
+            Item? selectedItem = null;
+            int quantity = 0;
+
+            Console.WriteLine("\n--------- YOUR CART ---------");
+            Console.WriteLine(" (empty)\n");
+            Console.WriteLine("--------------------------------------\n");
+
+            //################################################### USER INPUT ########################################################
+
+            Console.Write("Enter product ID: ");
+            int sid = Convert.ToInt32(Console.ReadLine());
+
+            foreach (Item item in items)
+            {
+                if (item.Id == sid)
+                {
+                    selectedItem = item;
+                    break;
+                }
+            }
+
+            if (selectedItem == null)
+            {
+                Console.WriteLine("Input ID not found");
+                return;
+            }
+
+            Console.Write("Enter quantity: ");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            if (quantity > selectedItem.Stock)
+            {
+                Console.WriteLine("Input quantity is more than current stock");
+                return;
+            }
+
+            selectedItem.Stock -= quantity;
+
+            //################################################### OUTPUT ########################################################
+
+            Console.WriteLine("\n--------- YOUR CART ---------");
+            Console.WriteLine($" Item: {selectedItem.Name} | Quantity: {quantity} | Total: PHP{selectedItem.Price * quantity}");
+
+            Console.WriteLine($"\n {selectedItem.Name} has been purchased");
         }
     }
 }
